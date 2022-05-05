d38f16ca8aec2f0fdfaf2d25d78d39486d3793d3
SCM-0914 <seanm0914@gmail.com>
2022-05-05 16:52:22 -0400

    Added user input and removed hard-coded number of process sets

--- src/main/java/DiscreteEventSim/maven/eclipse/App.java
+++ src/main/java/DiscreteEventSim/maven/eclipse/App.java
@@ -2,22 +2,36 @@ package DiscreteEventSim.maven.eclipse;
 
 import java.io.FileNotFoundException; 
 import java.util.*;
+import java.util.Scanner; 
   
 public class App { 
 	public static void main(String[] args) {   
 		
 		
-		ProcessGenerator.generatePoissonProcessSet("processSet0.json", 15, 15, 25);
-		ProcessGenerator.generatePoissonProcessSet("processSet1.json", 15, 15, 25);
-		ProcessGenerator.generatePoissonProcessSet("processSet2.json", 15, 15, 25);
-		ProcessGenerator.generatePoissonProcessSet("processSet3.json", 15, 15, 25);
-		ProcessGenerator.generatePoissonProcessSet("processSet4.json", 15, 15, 25);
+		
+		int numbOfSets = getInputInt("Number of process sets to simulate between 1 and 25?"
+				+ " \n (note: each process set is simulated 4 times, once for each CPU scheduling algorithm)", 1, 25);
+		int processesPerSet = getInputInt("Number of processes per set between 1 and 50?",1,50);
+		int poissonMean = getInputInt("This simulator gives processes different arrival times based on the Poisson distribution,"
+				+ "\n what is the mean arrival time between 5 and 100",5,100);
+		int maxBurst = getInputInt("max burst time per process between 5 and 50?",5,50);
+		int roundRobinQuantum = getInputInt("round robin requires a time quantum for simulation, what is the time quantum 1 and 25?",1,25);
+		
+		String excelFile = getInputString("Excel output file names (alphnumeric characters only)");
 		
 		
-		Process[][] procs = new Process[20][25];
+		
+		
+		
+		for(int i=0;i<numbOfSets;i++) {
+			ProcessGenerator.generatePoissonProcessSet("processSet"+i+".json", (double)poissonMean, maxBurst,processesPerSet);
+		}
+		
+		
+		Process[][] procs = new Process[numbOfSets*4][processesPerSet];
 		try {
 				for(int i=0;i<procs.length;i++) {
-					procs[i] = Process.readProcessesFromFile("processSet"+(i%5)+".json");
+					procs[i] = Process.readProcessesFromFile("processSet"+(i%numbOfSets)+".json");
 					Arrays.sort(procs[i],new SortByArrival());
 				}
 			
@@ -25,22 +39,31 @@ public class App {
 		catch (FileNotFoundException e) {e.printStackTrace();}
 		
 		
-		Process[][] setAnalysis = batchVariedSimulation(procs,5,5);
-		String[] labels = {
-				"FCFS","FCFS","FCFS","FCFS","FCFS",
-				"SJF","SJF","SJF","SJF","SJF",
-				"SJN","SJN","SJN","SJN","SJN",
-				"RR","RR","RR","RR","RR"};
+		Process[][] setAnalysis = batchVariedSimulation(procs,roundRobinQuantum,numbOfSets);
+		
+		
+		String[] labels = new String[numbOfSets*4];
+		for(int x=0;x<labels.length;x++) {
+			if(x<numbOfSets) {
+				labels[x] = "FCFS";
+			}else if(x<numbOfSets*2) {
+				labels[x] = "SJF";
+			}else if(x<numbOfSets*3) {
+				labels[x] = "SJN";
+			}else if(x<numbOfSets*4) {
+				labels[x] = "RR";
+			}
+			
+		}
 		
 		ExcelOutputHandler myBook = new ExcelOutputHandler();
-		myBook.recordBatchOutputs(setAnalysis,labels,5);
-		myBook.saveToFile("book");
+		myBook.recordBatchOutputs(setAnalysis,labels,numbOfSets);
+		myBook.saveToFile(excelFile);
 		
-		
+		System.out.println("\n\n PROGRAM OUTPUT IN: "+excelFile+".xslx");
 	}
 	
 	public static Process[][] batchSimulation(Process[][] multiQueue, int algorithm,int timeQuantum){
-		@SuppressWarnings("unchecked")
 		Process[][] finalizedProcesses = new Process[multiQueue.length][multiQueue[0].length];
 		for(int i=0;i<multiQueue.length;i++) {
 				finalizedProcesses[i] = simulation(multiQueue[i],algorithm,timeQuantum);
@@ -51,10 +74,8 @@ public class App {
 	public static Process[][] batchVariedSimulation(Process[][] multiQueue,int timeQuantum,int batchCount){
 		int count = 0;
 		int algorithm = 0;
-		@SuppressWarnings("unchecked")
 		Process[][] finalizedProcesses = new Process[multiQueue.length][multiQueue[0].length];
 		for(int i=0;i<multiQueue.length;i++) {
-				System.out.println(algorithm+" -- "+i);
 				finalizedProcesses[i] = simulation(multiQueue[i],algorithm,timeQuantum);
 				count++;
 				if(count==batchCount) {
@@ -156,7 +177,25 @@ public class App {
 	   
 	   
 		   
-		   
+	public static int getInputInt(String prompt,int min,int max) {
+		System.out.print("\n\n"+prompt+" >>> ");
+		Scanner scan = new Scanner(System.in);
+		int input = scan.nextInt();
+		if(input < min || input > max) {
+			input = getInputInt(prompt,min,max);
+		}
+		return input;
+	}   
+	
+	public static String getInputString(String prompt) {
+		System.out.print("\n\n"+prompt+" >>> ");
+		Scanner scan = new Scanner(System.in);
+		String input = scan.nextLine();
+		if(!input.matches("[A-Za-z0-9]*")) {
+			input = getInputString(prompt);
+		}
+		return input;
+	}  
 	
 
 
